6308ebc538b679a0b1f15ddeddbb26d9f0f61564
codeirawan <codeirawan@gmail.com>
2024-10-16 16:18:24 +0700

    fix: set temp cache

--- config/cache.php
+++ config/cache.php
@@ -49,7 +49,7 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
+            'path' => storage_path('tmp/framework/cache/data'),
         ],
 
         'memcached' => [
