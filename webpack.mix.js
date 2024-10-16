4c6154903166a5972097888d0ebe08c048693be6
codeirawan <codeirawan@gmail.com>
2024-10-16 23:07:59 +0700

    chore: setup vercel

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,21 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+// Combine all CSS styles into a single file
+mix.styles([
+    'public/css/custom.css',
+    'public/css/auth.css',
+    'public/css/404.css',
+    'public/css/datatable.css',
+    'public/css/form/wizard.css'
+], 'public/css/app.css');
 
-// Define your CSS and JS files here
-mix.styles(['public/css/custom.css'], 'public/css/app.css')
-   .js('resources/js/app.js', 'public/js/leaflet-geosearch.js')
-   .version();
+// Combine all JavaScript files
+mix.js('public/js/auth.js', 'public/js/app.js')
+   .js('public/js/form/validation.js', 'public/js/app.js')
+   .js('public/js/datatable.js', 'public/js/app.js')
+   .js('public/js/tooltip.js', 'public/js/app.js')
+   .js('public/js/form/thousand-separator.js', 'public/js/app.js')
+   .js('resources/js/app.js', 'public/js/app.js');
+
+// Versioning the files for cache busting
+mix.version();
